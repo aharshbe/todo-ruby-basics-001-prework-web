e954f2cf03fe255810725bc105a9cb8ad39b6815
iashy <iashy.ruby@gmail.com>
2016-02-23 00:37:17 -0800

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,54 @@
 def division(num1, num2)
+  
+  num1/num2
  
 end
 
+division(45,9)
+
 def assign_variable(value)
+ 
+ value 
   
 end
 
-def argue
+assign_variable('Bob')
+
+def argue(is)
+  
+  is
   
 end
 
-def greeting
+argue('fun')
 
+def greeting(hello, there)
+
+  hello+there
+  
 end
+
+greeting('Hi', 'There!')
+
+
 
 def return_a_value
+
+return 'Nice'
 
 end
 
 def last_evaluated_value
 
-end
-
-def pizza_party
+  return 'expert'
 
 end
+
+def pizza_party(cheese = 'cheese')
+  
+  cheese
+
+end
+pizza_party
+
+#this one was fun, lots to do!
